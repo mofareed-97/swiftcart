7bb2a12953730149d24538c93c24880afabc5f7c
mofareed-97 <m.fareeed1997@gmail.com>
2023-07-01 03:34:11 +0300

    product details

---
+++ src/app/(site)/products/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductDetails() {
+  return (
+    <div className="">
+      <div className="mx-auto max-w-[1500px] py-20">
+        <h1 className="font-heading text-5xl">Products</h1>
+      </div>
+    </div>
+  );
+}
